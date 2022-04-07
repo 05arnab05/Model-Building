153abac86bf6f117b9ab6e773e68bb9324f7911a
05arnab05 <arnabch5991@gmail.com>
2022-04-07 09:42:44 +0300

    Data logging additions

---
+++ All_models.py
@@ -0,0 +1,8 @@
+import pandas as pd
+Df_Concentration=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Concentration%20Only/Compiled_Comparative_Data_WithBosch.csv?token=GHSAT0AAAAAABSZJHLI6LK2Z5BNWPWVJOIYYR4PBZQ')
+Df_Humidity=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Humidity%20only/Compiled_Comparative_Data_WithBosch.csv')
+Df_Temperature=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Temperature%20only/Compiled_Comparative_Data_WithBosch.csv')
+
+def Preprocessing():
+    Df.Concentration['timestamp']=pd. df.Concencentration
+    
