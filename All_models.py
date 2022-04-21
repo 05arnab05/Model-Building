13269918c021cff0442ad23dd006130e0f6f6c75
05arnab05 <arnabch5991@gmail.com>
2022-04-21 13:57:46 +0300

    Datetime allocation in Code

--- All_models.py
+++ All_models.py
@@ -1,8 +1,18 @@
 import pandas as pd
-Df_Concentration=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Concentration%20Only/Compiled_Comparative_Data_WithBosch.csv?token=GHSAT0AAAAAABSZJHLI6LK2Z5BNWPWVJOIYYR4PBZQ')
-Df_Humidity=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Humidity%20only/Compiled_Comparative_Data_WithBosch.csv')
-Df_Temperature=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Temperature%20only/Compiled_Comparative_Data_WithBosch.csv')
+Concentration=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Concentration%20Only/Compiled_Comparative_Data_WithBosch.csv?token=GHSAT0AAAAAABSZJHLI6LK2Z5BNWPWVJOIYYR4PBZQ')
+Humidity=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Humidity%20only/Compiled_Comparative_Data_WithBosch.csv')
+Temperature=pd.read_csv('https://raw.githubusercontent.com/05arnab05/Model-Building/main/Indoor%20Fromaldehyde%20sensing%20data/Temperature%20only/Compiled_Comparative_Data_WithBosch.csv')
 
 def Preprocessing():
-    Df.Concentration['timestamp']=pd. df.Concencentration
+    global Concentration, Humidity, Temperature
+    Datasets= {"Concentration","Humidity","Temperature"}
+    for i in Datasets:
+        eval(i)['timestamp']=pd.to_datetime(eval(i)['timestamp'])
+        
     
+def main():
+    Preprocessing()
+    
+    
+if __name__ == '__main__':
+    main()
